ae48f1af7f411a842e810355abeea1132e0c5e99
Thanasak2003 <thanasuksongsriohm@gmail.com>
2025-03-31 20:01:01 +0700

    update docker

--- src/rider/rider.controller.spec.ts
+++ src/rider/rider.controller.spec.ts
@@ -57,7 +57,7 @@ describe('RiderController', () => {
     });
   });
 
-  
+
   describe('findOne', () => {
     it('should return a rider by ID', async () => {
       const result = { id: 1, firstName: 'John Doe', lastName: 'John Doe',email: 'john@gmail.com',licensePlate: 'XYZ-5555',phoneNumber: '089789798',createdAt: new Date(), updatedAt: new Date()};
@@ -66,58 +66,4 @@ describe('RiderController', () => {
       expect(await riderController.findOne('1')).toBe(result);
     });
   });
-
-  // describe('findRidersWithinRadius', () => {
-  //   it('should return riders within 5km radius', async () => {
-  //     const result = [{ id: 1, firstName: 'John Doe', lastName: 'John Doe',email: 'john@gmail.com',licensePlate: 'XYZ-5555',phoneNumber: '089789798',createdAt: new Date(), updatedAt: new Date() }];
-  //     jest.spyOn(riderService, 'findRidersWithinRadius').mockResolvedValue(result);
-
-  //     const latitude = 13.7563;
-  //     const longitude = 100.5018;
-
-  //     expect(await riderController.findRidersWithinRadius(latitude, longitude)).toBe(result);
-  //   });
-  // });
-
-
-  // describe('update', () => {
-  //   it('should update a rider by ID', async () => {
-  //     const updateRiderDto: UpdateRiderDto = { name: 'John Updated' };
-  //     const result = { id: 1, ...updateRiderDto };
-
-  //     jest.spyOn(riderService, 'update').mockResolvedValue(result);
-
-  //     expect(await riderController.update('1', updateRiderDto)).toBe(result);
-  //   });
-  // });
-
-  // describe('remove', () => {
-  //   it('should remove a rider by ID', async () => {
-  //     const result = { id: 1, name: 'John Doe' };
-  //     jest.spyOn(riderService, 'remove').mockResolvedValue(result);
-
-  //     expect(await riderController.remove('1')).toBe(result);
-  //   });
-  // });
-
-  // describe('findRiderLocation', () => {
-  //   it('should return the location of a rider', async () => {
-  //     const result = { id: 1, latitude: 13.7563, longitude: 100.5018 };
-  //     jest.spyOn(riderService, 'findRiderLocation').mockResolvedValue(result);
-
-  //     expect(await riderController.findRiderLocation('1')).toBe(result);
-  //   });
-  // });
-
-  // describe('CreateRiderLocation', () => {
-  //   it('should create a location for a rider', async () => {
-  //     const createLocationDto: CreateLocationDto = { latitude: 13.7563, longitude: 100.5018 };
-  //     const result = { id: 1, ...createLocationDto };
-
-  //     jest.spyOn(riderService, 'CreateRiderLocation').mockResolvedValue(result);
-
-  //     expect(await riderController.CreateRiderLocation('1', createLocationDto)).toBe(result);
-  //   });
-  // });
-
 });
